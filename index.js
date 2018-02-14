402d79ce04241cbc5fd827b412ecdc6095038349
Sandy Edwards <peterjamesedwards@gmail.com>
2018-02-14 17:29:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ function happyHolidays(string) {
 return "Happy holidays!"
 }
 
-function happyHolidaysTo(string) {
-  return "Happy holidays, ${string}!"
+function happyHolidaysTo(name) {
+  return ("Happy holidays, ${name}!")
 }
